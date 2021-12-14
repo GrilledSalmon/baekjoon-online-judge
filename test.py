3390f367b4fa8a3f2e3bbbdd5dfeca303ac28fb7
Yoonwoo Kye <mryoonwoo@naver.com>
2021-12-15 01:10:29 +0900

    1302 211214

--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-M = 6
+from collections import Counter
 
-set_1 = set([1])
-conn_left = []
+lst = [1, 2, 3, 4, 3, 2, 2, 2, 1, 0]
 
-conn_list = sorted([sorted(list(map(int, input().split()))) for _ in range(M)])
+cnt = Counter(lst)
 
-while True:
-    len_before = len(set_1)
-    print(conn_list)
-    for conn in conn_list:
-        n1, n2 = conn
-        if n1 in set_1 or n2 in set_1:
-            set_1.update([n1, n2])
-        else:
-            conn_left.append(conn)
-    
-    # for문을 돌고 set_1의 변화가 없을 경우(추가로 1과 연결된 요소가 없을 경우)
-    if len_before == len(set_1): 
-        break
-    else: # 처음 loop에서 선택받지 못한 애들 다시 돌려주기(1과 연결될 수 있는 새로운 가능성이 있으므로)
-        conn_list = conn_left
-
-print(len(set_1) - 1)
+for i in cnt.keys():
+    print(i, cnt[i])
