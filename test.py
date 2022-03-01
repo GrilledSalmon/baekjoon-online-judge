493325b38e4052cec950d42ee91a609848ba0ef9
Yoonwoo Kye <mryoonwoo@naver.com>
2022-03-01 11:20:29 +0900

    7569 토마토 220301

--- test.py
+++ test.py
@@ -1,29 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-miro = [list(map(int, list(input().rstrip()))) for _ in range(N)]
+def solve():
+    M, N, H = map(int, input().split())
+    tomato = [[list(map(int, input().split())) for _ in range(N)] for __ in range(H)]
 
-# BFS?
-def bfs():
-    queue = [(0, 0)]
-    miro[0][0] = 0
-    step = 1
-    while True:
+    ripen_tomato_coord = [] # 익은 토마토 위치
+    for z in range(H):
+        for y in range(N):
+            for x in range(M):
+                if tomato[z][y][x] == 1:
+                    ripen_tomato_coord.append((x, y, z))
+
+    # BFS
+    queue = ripen_tomato_coord
+    day = -1
+    while queue:
         temp = []
-        step += 1
-        for x, y in queue:
-            probe = []
-            if y != 0:   probe.append((x, y-1)) # 상
-            if y != N-1: probe.append((x, y+1)) # 하
-            if x != 0:   probe.append((x-1, y)) # 좌
-            if x != M-1: probe.append((x+1, y)) # 우
-            
-            for adj_x, adj_y in probe:
-                if (adj_x, adj_y) == (M-1, N-1):
-                    return step
-                elif miro[adj_y][adj_x]:
-                    miro[adj_y][adj_x] = 0
-                    temp.append((adj_x, adj_y))
+        day += 1
+        for x, y, z in queue:
+            probe = [(x-1, y, z), (x+1, y, z), (x, y-1, z), (x, y+1, z), (x, y, z-1), (x, y, z+1)]
+            for x_, y_, z_ in probe:
+                if 0<=x_<M and 0<=y_<N and 0<=z_<H and not tomato[z_][y_][x_]: # 좌표 만족 + 안 익었으면
+                    tomato[z_][y_][x_] = 1
+                    temp.append((x_, y_, z_))
         queue = temp
-print(bfs())
+    
+    for z in range(H):
+        for y in range(N):
+            for x in range(M):
+                if not tomato[z][y][x]: # 안 익은 토마토가 있다면
+                    print(-1)
+                    return
+
+    print(day)
+    return
+
+solve()
