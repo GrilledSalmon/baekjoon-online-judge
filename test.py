d3413176a85e301fd56e0820a78ae24a1ee15237
Yoonwoo Kye <mryoonwoo@naver.com>
2022-03-01 09:34:15 +0900

    1992 쿼드트리 220301

--- test.py
+++ test.py
@@ -1,42 +1,23 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+N = int(input())
+img = [input() for _ in range(N)]
 
-def D(n):
-    return 2*n % 10000
+def compr(x=0, y=0, n=N, img=img):
+    if n == 1:
+        return img[y][x]
+    
+    n //= 2
+    part1 = compr(x, y, n)
+    part2 = compr(x+n, y, n)
+    part3 = compr(x, y+n, n)
+    part4 = compr(x+n, y+n, n)
+    part = part1 + part2 + part3 + part4
+    if part == '0000':
+        return '0'
+    elif part == '1111':
+        return '1'
+    else:
+        return f'({part})'
 
-def S(n):
-    if n == 0:
-        return 9999
-    return n-1
-
-def L(n):
-    n_s = str(n).zfill(4)
-    new_n_s = n_s[1:] + n_s[0]
-    return int(new_n_s)
-
-def R(n):
-    n_s = str(n).zfill(4)
-    new_n_s = n_s[-1] + n_s[:-1]
-    return int(new_n_s)
-
-def BFS(A, B):
-    visited = [0]*10000
-    visited[A] = ' '
-    queue = deque([A])
-    while not visited[B]:
-        now_num = queue.popleft()
-        for i, command in enumerate([D, S, L, R]):
-            new_num = command(now_num)
-            if not visited[new_num]:
-                visited[new_num] = visited[now_num] + ['D', 'S', 'L', 'R'][i]
-                queue.append(new_num)
-    print(visited[B].strip())
-
-def solve():
-    T = int(input())
-    for _ in range(T):
-        A, B = map(int, input().split())
-        BFS(A, B)
-
-solve()
+print(compr())
