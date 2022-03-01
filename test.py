080279cb9c524c0300a630b82d198e17158b20db
Yoonwoo Kye <mryoonwoo@naver.com>
2022-03-01 10:27:23 +0900

    2178 미로 탐색 220301

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
 import sys
 input = sys.stdin.readline
-N = int(input())
-img = [input() for _ in range(N)]
 
-def compr(x=0, y=0, n=N, img=img):
-    if n == 1:
-        return img[y][x]
-    
-    n //= 2
-    part1 = compr(x, y, n)
-    part2 = compr(x+n, y, n)
-    part3 = compr(x, y+n, n)
-    part4 = compr(x+n, y+n, n)
-    part = part1 + part2 + part3 + part4
-    if part == '0000':
-        return '0'
-    elif part == '1111':
-        return '1'
-    else:
-        return f'({part})'
+N, M = map(int, input().split())
+miro = [list(map(int, list(input().rstrip()))) for _ in range(N)]
 
-print(compr())
+# BFS?
+def bfs():
+    queue = [(0, 0)]
+    miro[0][0] = 0
+    step = 1
+    while True:
+        temp = []
+        step += 1
+        for x, y in queue:
+            probe = []
+            if y != 0:   probe.append((x, y-1)) # 상
+            if y != N-1: probe.append((x, y+1)) # 하
+            if x != 0:   probe.append((x-1, y)) # 좌
+            if x != M-1: probe.append((x+1, y)) # 우
+            
+            for adj_x, adj_y in probe:
+                if (adj_x, adj_y) == (M-1, N-1):
+                    return step
+                elif miro[adj_y][adj_x]:
+                    miro[adj_y][adj_x] = 0
+                    temp.append((adj_x, adj_y))
+        queue = temp
+print(bfs())
